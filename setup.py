7eec73261d0e61214d6437afb32010592ee060bc
Jarrod Johnson <jbjohnso@us.ibm.com>
2013-06-28 10:35:41 -0400

    fix setup to include the private parts of the project

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(name='python-ipmi',
-      version='0.1.10',
+      version='0.1.11',
       description='Python IPMI implementation',
       author='Jarrod Johnson',
       author_email='jbjohnso@us.ibm.com',
       url='http://xcat.sf.net/',
       install_requires=['pycrypto'],
-      packages=['ipmi'],
+      packages=['ipmi','ipmi.private'],
      )
