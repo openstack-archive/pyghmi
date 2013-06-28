f333dbff8b8dc649c5aad9cd941eedd33ca63d54
Jarrod Johnson <jbjohnso@us.ibm.com>
2013-06-28 10:01:40 -0400

    Attempt to indicate pycrypto dependency

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from distutils.core import setup
 
 setup(name='python-ipmi',
-      version='0.1',
+      version='0.1.8',
       description='Python IPMI implementation',
       author='Jarrod Johnson',
       author_email='jbjohnso@us.ibm.com',
       url='http://xcat.sf.net/',
+      requires=['pycrypto'],
       packages=['ipmi'],
      )
