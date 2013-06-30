08d2e54d6871fcb4ae588e9a562cca0b9a311cba
Monty Taylor <mordred@inaugust.com>
2013-06-30 14:06:53 -0400

    Align to OpenStack Build Standards.
    Rearranged Sphinx.
    Put project under PBR control.
    Added tox file.
    Added .testr.conf file in anticipation of testr testing.
    
    Change-Id: I55180a6a3d224816c82999bf66156c7874786b17

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from setuptools import setup
+import setuptools
 
-setup(name='python-ipmi',
-      version='0.1.11',
-      description='Python IPMI implementation',
-      author='Jarrod Johnson',
-      author_email='jbjohnso@us.ibm.com',
-      url='http://xcat.sf.net/',
-      install_requires=['pycrypto'],
-      packages=['ipmi','ipmi.private'],
-     )
+setuptools.setup(
+    setup_requires=['d2to1>=0.2.10,<0.3', 'pbr>=0.5.16,<0.6'],
+    d2to1=True)
