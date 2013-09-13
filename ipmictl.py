781a7829be26bee0a9a78ca6ff49e0436be5a3f7
Monty Taylor <mordred@inaugust.com>
2013-09-13 11:21:12 -0500

    Update from requirements
    Trying to remove usage of d2to1 - it can cause breakages.
    Also, had to fix hacking 102. Turns out it expects copyright headers in
    comments, not docstrings.
    
    Change-Id: I3494349ffe2d4cba9c8bcb73408d60bbc12eff5e

--- ipmictl.py
+++ ipmictl.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
+# Copyright 2013 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 @author: Jarrod Johnson <jbjohnso@us.ibm.com>
-
-Copyright 2013 IBM Corporation
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 """
+
 """This is an example of using the library in a synchronous fashion. For now,
 it isn't conceived as a general utility to actually use, just help developers
 understand how the ipmi_command class workes.
