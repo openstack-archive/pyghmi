1bb2e11074b72912166d5ed01be3a7ec509b7a2f
Jarrod Johnson <jjohnson2@lenovo.com>
2015-04-23 15:19:25 -0400

    Create framework for OEM extensions (WIP)
    While the base IPMI specification is quite comprehensive,
    there are various points where OEM enhancement is possible.
    This can run the gamut from entirely distinct function
    (e.g. remote graphics) to additional 'sensors' to providing
    more interesting decode of 'extra' fields in FRU to decoding
    otherwise indecipherable SEL events.
    
    Change-Id: Iaf670f336f225d0ea00e1803eebb84104a78e8b3

---
+++ pyghmi/ipmi/oem/lenovo.py
@@ -0,0 +1,23 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2015 Lenovo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pyghmi.ipmi.oem.generic as generic
+
+class OEMHandler(generic.OEMHandler):
+    def __init__(self, oemid, ipmicmd):
+        # will need to retain data to differentiate
+        # variations.  For example System X versus Thinkserver
+        self.oemid = oemid
