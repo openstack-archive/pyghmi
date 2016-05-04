9fed1881bf47b6017813d191e6a97047873f163d
Jarrod Johnson <jjohnson2@lenovo.com>
2016-05-04 10:25:23 -0400

    Add Hardware inventory from Lenovo Agentless
    Lenovo System X systems offer up more inventory over agentless.
    Provide this data when the possibility presents itself.
    
    Change-Id: Ic377e951b5a6308157891f98dfdad6311337af48

--- pyghmi/ipmi/oem/lenovo/imm.py
+++ pyghmi/ipmi/oem/lenovo/imm.py
@@ -173,6 +173,85 @@ def fetch_agentless_firmware(ipmicmd, certverify):
         wc.request('GET', '/data/logout')
 
 
+def get_hw_inventory(ipmicmd, certverify):
+    hwmap = hardware_inventory_map(ipmicmd, certverify)
+    for key in hwmap:
+        yield (key, hwmap[key])
+
+
+def get_hw_descriptions(ipmicmd, certverify):
+    hwmap = hardware_inventory_map(ipmicmd, certverify)
+    for key in hwmap:
+        yield key
+
+
+def get_component_inventory(ipmicmd, certverify, compname):
+    hwmap = hardware_inventory_map(ipmicmd, certverify)
+    try:
+        return hwmap[compname]
+    except KeyError:
+        return None
+
+
+def hardware_inventory_map(ipmicmd, certverify):
+    hwmap = get_cached_data(ipmicmd, 'lenovo_cached_hwmap')
+    if hwmap:
+        return hwmap
+    hwmap = {}
+    wc = None
+    adapterdata = get_cached_data(ipmicmd, 'lenovo_cached_adapters')
+    if not adapterdata:
+        wc = get_web_session(ipmicmd, certverify, wc)
+        if wc:
+            adapterdata = wc.grab_json_response(
+                '/designs/imm/dataproviders/imm_adapters.php')
+            if adapterdata:
+                ipmicmd.ipmi_session.lenovo_cached_adapters = (
+                    adapterdata, _monotonic_time())
+    if adapterdata:
+        for adata in adapterdata['items']:
+            skipadapter = False
+            if not adata['adapter.oobSupported']:
+                continue
+            aslot = None
+            aname = adata['adapter.adapterName']
+            clabel = adata['adapter.connectorLabel']
+            if clabel == 'Unknown':
+                continue
+            if clabel != 'Onboard':
+                aslot = adata['adapter.slotNo']
+                if clabel == 'ML2':
+                    clabel = 'ML2 (Slot {0})'.format(aslot)
+                else:
+                    clabel = 'Slot {0}'.format(aslot)
+            bdata = {'location': clabel}
+            for fundata in adata['adapter.functions']:
+                bdata['pcislot'] = '{0:02x}:{1:02x}'.format(
+                    fundata['generic.busNo'], fundata['generic.devNo'])
+                serialdata = fundata.get('vpd.serialNo', None)
+                if (serialdata and serialdata != 'N/A' and
+                        '---' not in serialdata):
+                    bdata['serial'] = serialdata
+                partnum = fundata.get('vpd.partNo', None)
+                if partnum and partnum != 'N/A':
+                    bdata['partnumber'] = partnum
+                if 'network.pPorts' in fundata:
+                    for portinfo in fundata['network.pPorts']:
+                        for lp in portinfo['logicalPorts']:
+                            ma = lp['networkAddr']
+                            ma = ':'.join(
+                                [ma[i:i+2] for i in xrange(
+                                    0, len(ma), 2)]).lower()
+                            bdata['MAC Address {0}'.format(
+                                portinfo['portIndex'])] = ma
+                elif clabel == 'Onboard':  # skip the various onboard non-nic
+                    skipadapter = True
+            if not skipadapter:
+                hwmap[aname] = bdata
+        ipmicmd.ipmi_session.lenovo_cached_hwmap = (hwmap, _monotonic_time())
+    return hwmap
+
+
 def get_firmware_inventory(ipmicmd, bmcver, certverify):
     # First we fetch the system firmware found in imm properties
     # then check for agentless, if agentless, get adapter info using
