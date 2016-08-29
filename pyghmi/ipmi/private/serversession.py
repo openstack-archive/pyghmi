e0e81306890923303c27784a13c731aa87d9ac7e
Jarrod Johnson <jjohnson2@lenovo.com>
2016-08-29 16:16:00 -0400

    Fix BMC compatibility with Python 2.6
    BMC would try to push a bytearray at unpack.  In
    python 2.6, this does not work.  Explicitly denote
    changing to bytes to be compatible with python 2.6
    
    Change-Id: I1c919a0050cae9f90c50d89abb6a77774db81381

--- pyghmi/ipmi/private/serversession.py
+++ pyghmi/ipmi/private/serversession.py
@@ -274,7 +274,7 @@ class IpmiServer(object):
         headersum = ipmisession._checksum(*headerdata)
         header += bytearray(headerdata + [headersum, myaddr, mylun, 0x38])
         header += self.authcap
-        bodydata = struct.unpack('B' * len(header[17:]), header[17:])
+        bodydata = struct.unpack('B' * len(header[17:]), bytes(header[17:]))
         header.append(ipmisession._checksum(*bodydata))
         ipmisession._io_sendto(self.serversocket, header, sockaddr)
 
@@ -307,19 +307,20 @@ class IpmiServer(object):
                               bmc=self)
                 return
             data = data[13:]  # ditch 13 bytes so the payload works out
-        myaddr, netfnlun = struct.unpack('2B', data[14:16])
+        myaddr, netfnlun = struct.unpack('2B', bytes(data[14:16]))
         netfn = (netfnlun & 0b11111100) >> 2
         mylun = netfnlun & 0b11
         if netfn == 6:  # application request
             if data[19] == 0x38:  # cmd = get channel auth capabilities
-                verchannel, level = struct.unpack('2B', data[20:22])
+                verchannel, level = struct.unpack('2B', bytes(data[20:22]))
                 version = verchannel & 0b10000000
                 if version != 0b10000000:
                     return
                 channel = verchannel & 0b1111
                 if channel != 0xe:
                     return
-                (clientaddr, clientlun) = struct.unpack('BB', data[17:19])
+                (clientaddr, clientlun) = struct.unpack(
+                    'BB', bytes(data[17:19]))
                 level &= 0b1111
                 self.send_auth_cap(myaddr, mylun, clientaddr, clientlun,
                                    sockaddr)
