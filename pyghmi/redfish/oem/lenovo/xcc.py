f710b1d30a8eed19a9e86f01f9351c737666f3e5
Jarrod Johnson <jjohnson2@lenovo.com>
2019-04-16 15:27:14 -0400

    Add OEM extensibility framework to pyghmi redfish
    This enables OEM specific override for enhancements beyond the specification
    when avaible.
    
    Change-Id: I6a10f1fa58a425b2abfbeb9c85dd72c3b7d4cbba

---
+++ pyghmi/redfish/oem/lenovo/xcc.py
@@ -0,0 +1,32 @@
+# Copyright 2019 Lenovo Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pyghmi.redfish.oem.generic as generic
+
+
+class OEMHandler(generic.OEMHandler):
+    def get_description(self):
+        description = self._do_web_request('/DeviceDescription.json')
+        if description:
+            description = description[0]
+        u_height = description.get('u-height', '')
+        if not u_height and description.get(
+                'enclosure-machinetype-model', '').startswith('7Y36'):
+            u_height = '2'
+        if not u_height:
+            return {}
+        u_height = int(u_height)
+        slot = description.get('slot', '0')
+        slot = int(slot)
+        return {'height': u_height, 'slot': slot}
